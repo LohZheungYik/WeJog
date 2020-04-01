d06a98787cfbfba94983e1dcdb21cc7b05ca80f4
Hilman98 <62791261+Hilman98@users.noreply.github.com>
2020-04-01 16:13:53 +0800

    Splash.dart

--- lib/screens/splashScreen/Splash.dart
+++ lib/screens/splashScreen/Splash.dart
@@ -1,1 +1,17 @@
+
+import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:wejog1/screens/login/login.dart';
+
+class SplashScr extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+       'assets/splash.flr', //flare splash screen
+        Login(), //open login page after splash screen
+        startAnimation: 'Untitled',
+        backgroundColor: Color(0xff181818),
+    );
+  }
+}
+
