aa721db6e007bd45bfb7fa38e350dec829b7ef44
LohZheungYik <62550487+LohZheungYik@users.noreply.github.com>
2020-04-01 08:34:48 +0800

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wejog1/screens/splashScreen/SplashScr.dart';
+import 'package:wejog1/screens/splashScreen/Splash.dart';
 
 /*main function*/
 void main() => runApp(MyApp());
